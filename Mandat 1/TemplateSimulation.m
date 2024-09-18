c0d299dab03f3f5612e64bdd8cc1c140aec31ab5
simondesrochers <simon-3.desrochers@polymtl.ca>
2024-09-18 10:00:52 -0400

    sup cuh

--- Mandat 1/TemplateSimulation.m
+++ Mandat 1/TemplateSimulation.m
@@ -8,7 +8,7 @@
 
 %% Clear
 clear all; clc
-
+addpath("k-Wave/")
 %% Simulation grid parameter
 Nx = 124;               % number of grid points in the x (row) direction
 Ny = 60;               % number of grid points in the y (column) direction
@@ -17,8 +17,12 @@ dy = 5e-3;            % grid points spacing in the y direction [m]
 
 kgrid = kWaveGrid(Nx, dx, Ny, dy);
 
-SoundSpeed = 3000;      % Sound speed in the main material [m/s]
-Density = 2500;         % Density of the main material [kg/m^3]
+% SoundSpeed = 3000;      % Sound speed in the main material [m/s]
+% Density = 2500;         % Density of the main material [kg/m^3]
+
+% Glass pyrex
+SoundSpeed = 5640; % [m/s]
+Density = 2240; % [kg/m^3]
 
 
 %% Changement du pas de temps
@@ -67,10 +71,10 @@ end
 
 for i = 1:Nx
     for j = 1:Ny
-        if i > 70 && i < 80 && j >= (Ny/4-8) && j <= (Ny/4+8)
-            medium.sound_speed(i,j) = airSpeed;
-            medium.density(i,j) = airDensity;
-        elseif  j >= 25 && j <= 18 && i >= 45 && i <= 50 ||  j <= (24-i/2)
+        % if i > 70 && i < 80 && j >= (Ny/4-8) && j <= (Ny/4+8)
+        %     medium.sound_speed(i,j) = airSpeed;
+        %     medium.density(i,j) = airDensity;
+        if  j >= 25 && j <= 18 && i >= 45 && i <= 50 ||  j <= (24-i/2)
             medium.sound_speed(i,j) = airSpeed;
             medium.density(i,j) = airDensity;
         end
@@ -79,14 +83,14 @@ end
     
 % Ou similairement, des coupes.
 
-for i = 1:Nx
-    for j = 1:Ny
-        if i > 70 && i < 80 && j >= (Ny/4-8) && j <= (Ny/4+8)
-            medium.sound_speed(i,j) = airSpeed;
-            medium.density(i,j) = airDensity;
-        end
-    end
-end
+% for i = 1:Nx
+%     for j = 1:Ny
+%         if i > 70 && i < 80 && j >= (Ny/4-8) && j <= (Ny/4+8)
+%             medium.sound_speed(i,j) = airSpeed;
+%             medium.density(i,j) = airDensity;
+%         end
+%     end
+% end
 
 %% Define sensor
 % On définit les capteurs de l'onde acoustique. On peut en definir
@@ -95,8 +99,16 @@ end
 
 clear sensor
 
-sensorXGrid = [48];     % [gridPoint]
-sensorYGrid = [48];     % [gridPoint]
+% sensorXGrid = [48];     % [gridPoint]
+% sensorYGrid = [48];     % [gridPoint]
+% Nx = 124;               
+% Ny = 60; 
+
+x = round(linspace(5,119,24));
+y = round(linspace(5,55,12));
+[X,Y] = meshgrid(x,y);
+sensorXGrid = reshape(X,[],1);
+sensorYGrid = reshape(Y,[],1);
 
 % On transforme les points de la grille de simulation en position
 % cartésienne.
@@ -104,13 +116,13 @@ sensorYGrid = [48];     % [gridPoint]
 sensor.mask = [kgrid.x_vec(sensorXGrid)'; kgrid.y_vec(sensorYGrid)'];
 
 %% Source definition
-% De même, la source est défini grâce à une paire de point sur la grille de
+% De même, la source est définie grâce à une paire de point sur la grille de
 % simulation. On crée un disque de la taille d'une doigt pour l'impact.
 % Attention à ce que toute la source soit à l'intérieur du matériau simulé.
 % Dans le cas présent, la source ne doit pas être dans un périmètre de 4dx
 % du bord.
 
-sourceGrid = [91, 12];
+sourceGrid = [40, 12]; % Sera utilisé comme capteur apres la formation du dictionnaire
 source_radius = floor(0.01/dx);         % [grid points] (Taille d'un doigt)
 source_magnitude = 10;                  % [Pa]
 source_1 = source_magnitude*makeDisc(Nx, Ny, sourceGrid(1), sourceGrid(2), source_radius);
@@ -128,8 +140,8 @@ source_y_pos = kgrid.y_vec(sourceGrid(2));         % [grid points]
 
 figure;
 imagesc(kgrid.y_vec*1e3, kgrid.x_vec*1e3, medium.sound_speed); axis image
-ylabel('y - position [mm]')
-xlabel('x - position [mm]')
+ylabel('x - position [mm]')
+xlabel('y - position [mm]')
 c = colorbar;
 c.Label.String = 'Speed of sound';
 hold on;
@@ -151,11 +163,60 @@ xlabel('Temps [ms]')
 ylabel('Amplitude')
 title("Amplitude de l'onde mesuree au capteur")
 
+%% Formation du dictionnaire
+dic = flip(sensor_data,2); % retournement temporel
 
+%% Simu avec la source du dictionnaire comme senseur et avec une source random
+% Define sensor
+clear sensor
+sensorX = sourceGrid(1);     % [gridPoint]
+sensorY = sourceGrid(2);     % [gridPoint]
+
+% On transforme les points de la grille de simulation en position
+% cartésienne.
+sensor.mask = [kgrid.x_vec(sensorX)'; kgrid.y_vec(sensorY)'];
+
+% Random source definition
+randSourceGrid = [95, 34]; 
+source_radius = floor(0.01/dx);         % [grid points] (Taille d'un doigt)
+source_magnitude = 10;                  % [Pa]
+source_1 = source_magnitude*makeDisc(Nx, Ny, randSourceGrid(1), randSourceGrid(2), source_radius);
+source.p0 = source_1;
+
+% Pour l'affichage, on transforme les points de la grille de simulation en
+% position cartésienne. 
+source_x_pos = kgrid.x_vec(randSourceGrid(1));         % [grid points]
+source_y_pos = kgrid.y_vec(randSourceGrid(2));         % [grid points]
+
+%% Visualisation de la grille de simulation
+figure;
+imagesc(kgrid.y_vec*1e3, kgrid.x_vec*1e3, medium.sound_speed); axis image
+ylabel('x - position [mm]')
+xlabel('y - position [mm]')
+c = colorbar;
+c.Label.String = 'Speed of sound';
+hold on;
+plot(sensor.mask(2,:)*1e3, sensor.mask(1,:)*1e3, 'r.')
+plot(source_y_pos*1e3, source_x_pos*1e3, 'b+')
+legend('Sensor', 'Source')
+
+%% Simulation
+sensor_data = kspaceFirstOrder2D(kgrid, medium, source, sensor,...
+    'PMLSize', 2, 'PMLInside', false, 'DataCast', 'single');
+
+%% Correlation map
+corr = zeros(1,length(sensorXGrid));
+for i = 1:length(sensorXGrid)
+    corr(i) = max(xcorr(sensor_data,dic(i,:),'normalized'));
+end
+corr_mat = reshape(corr,size(X,2),size(X,1)); %DROLE DE METHODE MONOME
+figure;
+imagesc(corr_mat); axis image; colormap('gray');
+c = colorbar;
+c.Label.String = 'Correlation';
 %% Sauvegarde des données
 % Sauvegarder les données pour les réutiliser plus tard, où pour les
 % exporter vers python. Pour load dans python, utiliser scipy.io.loadmat()
 
 save('Sim1.mat', 'sensor_data')
-
 
