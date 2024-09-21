9e2119e7939a329c7bdef44d680bd2be06f6925c
charles pageot <pageotcharles@gmail.com>
2024-09-21 15:24:08 -0400

    Scripts to calculate contrast and resolution from the correlation matrices

---
+++ Mandat 1/compute_metrics_procedure.py
@@ -0,0 +1,65 @@
+from get_metrics import get_metrics, load_from_mat
+import numpy as np
+import os
+import csv
+
+# Process the correlation matrices and calculate the metrics
+
+# Define the mapping for the materials
+mat = {
+        'pin' : 'Pin',
+        'plexi' : 'Plexiglass',
+        'pvc' : 'PVC',
+}
+
+folder_path = 'Mandat 1\correlation_matrices'
+
+data = [
+    ['Combinasion of parameters', 'Mean resolution (mm)', 'Resolution uncertainty (mm)', 'Mean contrast (mm)', 'Contrast uncertainty (mm)'],
+    ]
+
+# Iterate through all files in the folder
+for filename in os.listdir(folder_path):
+    if filename.endswith('.mat') and os.path.isfile(os.path.join(folder_path, filename)):
+        print(f'Processing {filename}...')
+        filename_without_ext = os.path.splitext(filename)[0]
+
+        # Load the correlation matrix
+        corr_matrices = load_from_mat(os.path.join(folder_path, filename))
+        print(f'{filename_without_ext} loaded')
+        contrast_list = []
+        resolution_list = []
+
+        # Calculate the metrics
+        for i in range(corr_matrices.shape[2]):
+            corr_mat = corr_matrices[:,:,i]
+            resolution, contrast = get_metrics(corr_mat)
+            print('Done')
+            resolution_list.append(resolution)
+            contrast_list.append(contrast)
+        
+        mean_contrast = np.mean(contrast_list)
+        std_contrast = np.std(contrast_list)
+        contrast_uncertainty = std_contrast / np.sqrt(len(contrast_list))
+
+        mean_resolution = np.mean(resolution_list)
+        std_resolution = np.std(resolution_list)
+        resolution_uncertainty = std_resolution / np.sqrt(len(resolution_list))
+
+        mat_name, forme_name, sensor = filename_without_ext.split('_')
+
+        data.append([f'{mat[mat_name]}, {forme_name} et position {sensor}', mean_resolution, resolution_uncertainty, mean_contrast, contrast_uncertainty])
+        print('-'*50)
+
+# Open the file in write mode and write the data
+with open('metrics.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    
+    # Write each row of data
+    writer.writerows(data)
+
+
+
+
+
+
