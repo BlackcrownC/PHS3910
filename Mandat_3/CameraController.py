969f4a34172a99be2dfef10d2996b17ed7f3af7d
cedryk <cedrykjutras@hotmail.com>
2024-11-20 11:38:31 -0500

    yeet

--- Mandat_3/CameraController.py
+++ Mandat_3/CameraController.py
@@ -19,9 +19,11 @@ class CameraController:
     def __init__(self):
         self.camera: Any = None
         self.sdk: Any = None
+        self.img_ext = "tiff"
+        self.vid_ext = "avi"
 
     def init_values(self):
-        self.camera.exposure_time_us = 0.1  # Exemple de temps d'exposition de 25 ms (tester sur Thorlab)
+        self.camera.exposure_time_us = 30000  # Exemple de temps d'exposition de 25 ms (tester sur Thorlab)
         self.camera.black_level = 5
         self.camera.gain = 0
         self.camera.frames_per_trigger_zero_for_unlimited = 1  # Mode de capture continue : 0 ; Mode de capture ponctuel : 1
@@ -43,6 +45,7 @@ class CameraController:
                 exit()
             self.camera = self.sdk.open_camera(available_cameras[0])
             print(f"Caméra connectée avec succès : {available_cameras[0]}")
+            self.init_values()
             return self
         except TLCameraError as e:
             print(f"Erreur SDK : {e}")
@@ -50,8 +53,6 @@ class CameraController:
             # Nettoyer le SDK correctement
             if 'sdk' in locals():
                 self.sdk.dispose()
-
-        self.init_values()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.camera.disarm()
@@ -77,7 +78,7 @@ class CameraController:
             cv2.waitKey(0)  # Attendez une touche pour fermer la fenêtre
 
             # Enregistrer l'image en format TIFF
-            cv2.imwrite(f"{output_path}.tiff", image)
+            cv2.imwrite(f"{output_path}.{self.img_ext}", image)
             print(f"Image enregistrée sous : {output_path}")
         else:
             print("Aucune image capturée.")
@@ -94,36 +95,40 @@ class CameraController:
         frame_width = self.camera.image_width_pixels
         frame_height = self.camera.image_height_pixels
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        out = cv2.VideoWriter(f"{output_path}.avi", fourcc, nombre_image_par_seconde, (frame_width, frame_height), isColor=False)
+        out = cv2.VideoWriter(f"{output_path}.{self.vid_ext}", fourcc, nombre_image_par_seconde,
+                              (frame_width, frame_height), isColor=False)
 
         for picture_number in range(int(nombre_image_par_seconde * record_time)):
-            # Commencer à capturer des imagesc
             self.camera.issue_software_trigger()
-            time.sleep(1 / nombre_image_par_seconde)
+            # Commencer à capturer des imagesc
+            time.sleep(1/nombre_image_par_seconde)
             print("Capture d'images en cours...")
             frame = self.camera.get_pending_frame_or_null()
-            # print(frame.frame_count)
 
             if frame is not None:
                 print(f"Image capturée avec succès : {frame.frame_count}")
-                image = frame.image_buffer  # Assurez-vous que cet attribut est correct
                 # cv2.imshow('Image Capturee', image)  # Affichez l'image
                 # cv2.waitKey(0)  # Attendez une touche pour fermer la fenêtre
 
                 # Enregistrer l'image en format video
                 image = np.array(frame.image_buffer, dtype=np.uint8).reshape((frame_height, frame_width))
                 out.write(image)
-                # filename = fr"C:\Users\User\Desktop\Polyautomne2024\Laboratoire Lucien\Test_pictures\image_videopourlucien.tiff"
-                # cv2.imwrite(filename, image)
-                # print(f"Image enregistrée sous : {filename}")
             else:
-                print("Aucune image capturée.")
+                print("Unable to acquire image, program exiting...")
 
         # Désarmer et fermer la caméra après utilisation
         self.camera.disarm()
         print("Caméra désarmée et fermée.")
 
+    def get_image(self, filename: str = "test", is_video: bool = False):
+        if is_video:
+            image = cv2.VideoCapture(f"{filename}.{self.vid_ext}")
+            return image
+        image = cv2.imreadmulti(f"{filename}.{self.img_ext}", flags=cv2.IMREAD_LOAD_GDAL)[1]
+        image = np.float32(np.array(image))
+        return image
+
 
 with CameraController() as camera_controller:
-    camera_controller.capture()
+    # camera_controller.capture()
     camera_controller.record_video(10, 10, "test")
